3fe24ac5b4c3a2f781826a972bb2270181a91872
임세현 <sh00710000@gmail.com>
2022-08-08 22:18:07 +0900

    style: SIMT-5 compose theme 설정

---
+++ core-design-system/src/main/java/com/comit/core_design_system/theme/Theme.kt
@@ -0,0 +1,55 @@
+package com.comit.core_design_system.theme
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+@SuppressLint("ConflictingOnColor")
+private val LightColorPalette = lightColors(
+    primary = SimTongColor.Primary,
+    secondary = SimTongColor.MainColor,
+    background = SimTongColor.White,
+    surface = SimTongColor.White,
+    error = SimTongColor.Error,
+    onPrimary = SimTongColor.White,
+    onSecondary = SimTongColor.Primary,
+    onBackground = SimTongColor.Primary,
+    onSurface = SimTongColor.Primary,
+    onError = SimTongColor.White
+)
+
+private val DarkColorPalette = darkColors(
+    primary = SimTongColor.Primary,
+    secondary = SimTongColor.MainColor,
+    background = SimTongColor.Primary,
+    surface = SimTongColor.Primary,
+    error = SimTongColor.Error,
+    onPrimary = SimTongColor.White,
+    onSecondary = SimTongColor.Primary,
+    onBackground = SimTongColor.White,
+    onSurface = SimTongColor.White,
+    onError = SimTongColor.White
+)
+
+
+@Composable
+fun SimTongTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable() () -> Unit
+) {
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
+
+    MaterialTheme(
+        colors = colors,
+        shapes = RoundShapes,
+        content = content
+    )
+}
