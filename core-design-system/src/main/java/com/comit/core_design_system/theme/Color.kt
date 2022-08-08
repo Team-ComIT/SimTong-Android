7464544fa9ab353974173c33bdc3cf03c00fe1ea
임세현 <sh00710000@gmail.com>
2022-08-08 22:18:45 +0900

    style SIMT-5 shape & color

---
+++ core-design-system/src/main/java/com/comit/core_design_system/theme/Color.kt
@@ -0,0 +1,37 @@
+package com.comit.core_design_system.theme
+
+import androidx.compose.ui.graphics.Color
+
+object SimTongColor {
+    val White = Color(0xFFFFFFFF)
+    val Black = Color(0xFF000000)
+    val Primary = Color(0xFF242424)
+
+    val FocusBlue = Color(0xFF006EFF)
+    val Check = Color(0xFF04DF00)
+    val Error = Color(0xFFDC3035)
+
+    val Gray100 = Color(0xFFFDFDFD)
+    val Gray200 = Color(0xFFEDEDED)
+    val Gray300 = Color(0xFFD3D3D3)
+    val Gray400 = Color(0xFFBDBDBD)
+    val Gray500 = Color(0xFFA7A7A7)
+    val Gray600 = Color(0xFF929292)
+    val Gray700 = Color(0xFF7C7C7C)
+    val Gray800 = Color(0xFF505050)
+
+    val SubColor100 = Color(0xFFED666A)
+    val SubColor200 = Color(0xFFC32328)
+
+    val MainColor = Color(0xFFE84045)
+
+    val MainColor100 = Color(0xFFFFEDEE)
+    val MainColor200 = Color(0xFFFFC9CB)
+    val MainColor300 = Color(0xFFFF999C)
+    val MainColor400 = Color(0xFFED666A)
+    val MainColor500 = Color(0xFFE84045)
+    val MainColor600 = Color(0xFFDC3035)
+    val MainColor700 = Color(0xFFC32328)
+    val MainColor800 = Color(0xFF971D20)
+    val MainColor900 = Color(0xFF4D0002)
+}
