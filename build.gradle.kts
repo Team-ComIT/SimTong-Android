a50ef81e79bed99c596780c1cf8e590266f6d135
임세현 <sh00710000@gmail.com>
2022-08-14 22:02:42 +0900

    chore: set detekt

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,25 @@
 plugins {
     id(BuildPlugins.KT_LINT) version Versions.KT_LINT
+    id("io.gitlab.arturbosch.detekt") version "1.21.0"
+}
+
+dependencies {
+    detekt ("io.gitlab.arturbosch.detekt:detekt-formatting:1.21.0")
 }
 
 allprojects {
+
     apply {
         plugin("org.jlleitschuh.gradle.ktlint")
+        plugin("io.gitlab.arturbosch.detekt")
+    }
+
+    afterEvaluate {
+        detekt {
+            autoCorrect = true
+            buildUponDefaultConfig = true
+            config.setFrom(files("$rootDir/detekt-config.yml"))
+        }
     }
 }
 
