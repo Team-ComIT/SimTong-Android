77b093976d473fd8ad0d12ed4ffad3e6d61e44f6
kimjjunho <kimjjun1007@gmail.com>
2022-08-15 01:38:57 +0900

    fix: SIMT-6 place component 프리뷰 추가 및 수정

--- core-design-system/src/main/java/com/comit/core_design_system/component/PlaceComponent.kt
+++ core-design-system/src/main/java/com/comit/core_design_system/component/PlaceComponent.kt
@@ -2,36 +2,34 @@ package com.comit.core_design_system.component
 
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.comit.core_design_system.R
 import com.comit.core_design_system.theme.Body5
 import com.comit.core_design_system.theme.SimTongColor
 
-/*modifier = Modifier
-.padding(0.7.dp, 15.dp, 0.7.dp, 0.dp)
-.fillMaxWidth()
-.height(200.dp)
-.background(
-color = Color.White,
-shape = RoundedCornerShape(20.dp)*/
 
 @Composable
 fun ChoosePlace(
     modifier: Modifier = Modifier,
-    onClick: (Int) -> Unit,
+    onClick: (Int) -> Unit = {},
     list: List<String>,
     painter: Painter = painterResource(id = R.drawable.ic_check)
 ) {
@@ -81,7 +79,7 @@ fun ChoosePlaceItem(
         modifier = Modifier
             .padding(0.dp, 20.dp, 0.dp, 0.dp)
             .fillMaxWidth()
-            .height(24.dp)
+            .height(35.dp)
             .clickable {
                 onClick(index)
                 isNeedExpansion.value = index
@@ -90,6 +88,8 @@ fun ChoosePlaceItem(
         Body5(
             text = text,
             modifier = Modifier
+                .fillMaxHeight()
+                .wrapContentHeight(CenterVertically)
                 .padding(24.dp, 0.dp, 0.dp, 0.dp)
         )
 
@@ -110,7 +110,24 @@ fun CheckImage(isNeedExpansion: Int, painter: Painter, index: Int) {
             modifier = Modifier
                 .fillMaxWidth()
                 .wrapContentWidth(align = Alignment.End)
+                .fillMaxHeight()
+                .wrapContentHeight(CenterVertically)
                 .padding(0.dp, 0.dp, 24.dp, 0.dp)
         )
     }
+}
+
+@Preview
+@Composable
+fun PlaceComponent(){
+    ChoosePlace(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(200.dp)
+            .background(
+                color = Color.White,
+                shape = RoundedCornerShape(20.dp)
+            )
+            .padding(0.7.dp, 5.dp, 0.7.dp, 0.dp),
+        list = listOf("\uD83D\uDC5C      전체 지점","\uD83D\uDCBB      근무 지점"))
 }
