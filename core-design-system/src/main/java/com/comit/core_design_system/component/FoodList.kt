10c625e1b3ac70fa5cc57cea09374a57a31ae631
kimjjunho <kimjjun1007@gmail.com>
2022-08-16 19:36:32 +0900

    fix: SIMT-6 food list 디자인 변경 및 프리뷰 추가

--- core-design-system/src/main/java/com/comit/core_design_system/component/FoodList.kt
+++ core-design-system/src/main/java/com/comit/core_design_system/component/FoodList.kt
@@ -1,98 +1,76 @@
 package com.comit.core_design_system.component
 
-import androidx.compose.foundation.border
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.comit.core_design_system.theme.Body9
-import com.comit.core_design_system.theme.OtherColor
-import com.comit.core_design_system.theme.SimTongColor
-import com.comit.core_design_system.theme.notoSansFamily
+import com.comit.core_design_system.R
+import com.comit.core_design_system.theme.*
 import java.text.SimpleDateFormat
 import java.util.*
 
-data class FoodListDataSample(
-    val time: String,
-    val menu: String
-)
-
 @Composable
-fun FoodListLazyRow(list: List<FoodListDataSample>) {
-    LazyRow() {
-        val currentTime = Calendar.getInstance().time
-        val date = SimpleDateFormat("yyyy.MM.dd (EE)", Locale.KOREA).format(currentTime)
+fun FoodListLazyRow(list: List<String>) {
+    val time = SimpleDateFormat("hhmm").format(Date(System.currentTimeMillis())).toInt()
+    var timeCheck = 0
 
-        items(list) {
-            FoodListItem(foodListDataSample = it, date = date)
+    if(time > 1500){
+        timeCheck = 2
+    }
+    else if(time > 1000){
+        timeCheck = 1
+    }
+
+    LazyRow() {
+        itemsIndexed(list) {index,it->
+            FoodListItem(index, it, timeCheck)
         }
     }
 }
 
 @Composable
-fun FoodListItem(foodListDataSample: FoodListDataSample, date: String) {
+fun FoodListItem(index: Int, menu: String, timeCheck: Int) {
+
+    val textColor: Color = if(timeCheck == index) SimTongColor.White else SimTongColor.Black
+    val backgroundImage: Int = if(timeCheck == index) R.drawable.img_food_red else R.drawable.img_food
+
     Card(
         shape = RoundedCornerShape(10.dp),
+        backgroundColor = Color(0xFF00FF0000),
         modifier = Modifier
-            .width(150.dp)
+            .width(140.dp)
             .height(160.dp)
             .padding(15.dp, 0.dp, 15.dp, 0.dp)
-            .border(
-                width = 2.dp,
-                color = SimTongColor.MainColor,
-                shape = RoundedCornerShape(10.dp)
-            )
     ) {
-        Column(modifier = Modifier.fillMaxSize()) {
-            Card(
-                shape = RoundedCornerShape(10.dp),
-                backgroundColor = SimTongColor.MainColor,
+        Box(modifier = Modifier.fillMaxSize()){
+            Image(
+                painter = painterResource(id = backgroundImage),
+                contentDescription = "",
                 modifier = Modifier
-                    .padding(10.dp, 13.dp, 0.dp, 0.dp)
-            ) {
-                Text(
-                    text = foodListDataSample.time,
-                    fontSize = 10.sp,
-                    fontFamily = notoSansFamily,
-                    fontWeight = FontWeight.SemiBold,
-                    color = Color.White,
-                    modifier = Modifier
-                        .width(40.dp)
-                        .wrapContentWidth(align = Alignment.CenterHorizontally)
-                        .height(18.dp)
-                        .wrapContentHeight(align = Alignment.CenterVertically)
-                )
-            }
+                    .fillMaxSize()
+            )
 
-            Body9(
-                text = foodListDataSample.menu,
+            Body13(
+                text = menu,
+                color = textColor,
                 modifier = Modifier
                     .height(96.dp)
                     .padding(10.dp, 10.dp, 10.dp, 0.dp)
             )
         }
-
-        Text(
-            text = date,
-            color = OtherColor.GrayB3,
-            fontSize = 9.sp,
-            fontFamily = notoSansFamily,
-            fontWeight = FontWeight.SemiBold,
-            modifier = Modifier
-                .fillMaxHeight()
-                .wrapContentHeight(align = Alignment.Bottom)
-                .fillMaxWidth()
-                .wrapContentWidth(align = Alignment.End)
-                .padding(0.dp, 0.dp, 10.dp, 7.dp)
-        )
     }
+}
+
+@Preview
+@Composable
+fun FoodList(){
+    FoodListLazyRow(list = listOf("누룽지","돼지불고기","배추김치"))
 }
