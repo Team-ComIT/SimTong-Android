1d1ef1357c2977b874faa4861d0372522d8c18f7
kimjjunho <kimjjun1007@gmail.com>
2022-10-03 08:22:23 +0900

    refactor: SIMT-18 carrot market 규격 및 이미지 수정

--- core-design-system/src/main/java/com/comit/core_design_system/component/CarrotMarket.kt
+++ core-design-system/src/main/java/com/comit/core_design_system/component/CarrotMarket.kt
@@ -1,21 +1,24 @@
 package com.comit.core_design_system.component
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Stable
 import androidx.compose.runtime.mutableStateOf
@@ -23,18 +26,15 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.comit.common.compose.noRippleClickable
 import com.comit.core_design_system.R
 import com.comit.core_design_system.icon.SimTongIcons
 import com.comit.core_design_system.theme.Body10
+import com.comit.core_design_system.theme.Body3
 import com.comit.core_design_system.theme.Body6
 import com.comit.core_design_system.theme.SimTongColor
-import com.comit.core_design_system.theme.notoSansFamily
 import java.text.DecimalFormat
 
 data class CarrotMarketData(
@@ -42,14 +42,14 @@ data class CarrotMarketData(
     val place: String,
     val time: String,
     val price: Int,
-    val like: Boolean
+    val like: Boolean,
+    val imageUrl: String?
 )
 
 @Composable
-fun CarrotMarketLazyVerticalGrid(
+fun CarrotMarket(
     modifier: Modifier = Modifier,
     list: List<CarrotMarketData>,
-    imageHeight: Dp = 150.dp,
     heartClick: () -> Unit = {},
     cardClick: () -> Unit = {}
 ) {
@@ -58,7 +58,6 @@ fun CarrotMarketLazyVerticalGrid(
             CarrotMarketItemCard(
                 modifier = modifier,
                 data = it,
-                imageHeight = imageHeight,
                 heartClick = heartClick,
                 cardClick = cardClick
             )
@@ -70,7 +69,6 @@ fun CarrotMarketLazyVerticalGrid(
 fun CarrotMarketItemCard(
     modifier: Modifier,
     data: CarrotMarketData,
-    imageHeight: Dp,
     heartClick: () -> Unit,
     cardClick: () -> Unit
 ) {
@@ -79,61 +77,82 @@ fun CarrotMarketItemCard(
 
     Card(
         modifier = modifier
-            .width(150.dp)
-            .height(255.dp)
             .clickable { cardClick() }
     ) {
-        Column(modifier = Modifier.fillMaxSize()) {
-            Image(
-                painterResource(id = R.drawable.img_item_carrot),
-                contentDescription = "item image",
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(20.dp, 0.dp, 20.dp, 0.dp)
+        ) {
+            
+            Box(
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .height(imageHeight)
-            )
+                    .background(
+                        color = SimTongColor.Gray300,
+                        shape = RoundedCornerShape(5.dp)
+                    )
+            ) {
+                if(data.imageUrl == null){
+                    Image(
+                        painterResource(id = R.drawable.ic_carrot_market_base),
+                        contentDescription = "item image",
+
+                        modifier = Modifier
+                            .padding(50.dp)
+                    )
+                }else{
+
+                }
+            }
 
             Body6(
                 text = data.productName,
                 color = SimTongColor.Black,
-                modifier = Modifier.padding(20.dp, 15.dp, 0.dp, 0.dp)
+                modifier = Modifier.padding(0.dp, 10.dp, 0.dp, 0.dp)
             )
 
             Body10(
                 text = data.place + " " + "•" + " " + data.time,
                 color = SimTongColor.OtherColor.GrayA,
-                modifier = Modifier.padding(20.dp, 3.dp)
+                modifier = Modifier.padding(0.dp, 2.dp,0.dp,9.dp)
             )
 
-            val decimalFormat = DecimalFormat("#,###")
-            Text(
-                text = decimalFormat.format(data.price),
-                color = SimTongColor.Black,
-                fontSize = 15.sp,
-                fontFamily = notoSansFamily,
-                fontWeight = FontWeight.SemiBold,
+            Row(
                 modifier = Modifier
-                    .fillMaxHeight()
-                    .wrapContentHeight(align = Alignment.Bottom)
-                    .padding(20.dp, 0.dp, 0.dp, 25.dp)
-            )
+                    .height(21.dp)
+                    .padding(0.dp, 0.dp, 0.dp, 0.dp)
+            ) {
+                val decimalFormat = DecimalFormat("#,###")
+
+                Body3(
+                    text = decimalFormat.format(data.price),
+                    color = SimTongColor.Black,
+                    modifier = Modifier
+                        .fillMaxHeight()
+                        .wrapContentHeight(Alignment.CenterVertically)
+                )
+
+                Image(
+                    painter = painterResource(
+                        id = SimTongIcons.Heart(heartClickCheck.value)
+                    ),
+                    contentDescription = "heart image",
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .wrapContentWidth(align = Alignment.End)
+                        .noRippleClickable {
+                            heartClickCheck.value = !heartClickCheck.value
+                            heartClick()
+                        }
+                )
+            }
+            
+            Spacer(modifier = Modifier.height(24.dp))
+            
         }
 
-        Image(
-            painter = painterResource(
-                id = SimTongIcons.Heart(heartClickCheck.value)
-            ),
-            contentDescription = "heart image",
-            modifier = Modifier
-                .fillMaxHeight()
-                .wrapContentHeight(align = Alignment.Bottom)
-                .fillMaxWidth()
-                .wrapContentWidth(align = Alignment.End)
-                .padding(0.dp, 0.dp, 20.dp, 25.dp)
-                .noRippleClickable {
-                    heartClickCheck.value = !heartClickCheck.value
-                    heartClick()
-                }
-        )
+
+
     }
 }
 
@@ -142,36 +161,40 @@ private val CarrotMarketPrice: Int = 100000
 
 @Preview
 @Composable
-fun CarrotMarket() {
-    CarrotMarketLazyVerticalGrid(
+fun PreviewCarrotMarket() {
+    CarrotMarket(
         list = listOf(
             CarrotMarketData(
                 "제품",
                 "본점",
                 "1시간 전",
                 CarrotMarketPrice,
-                false
+                false,
+                null
             ),
             CarrotMarketData(
                 "제품",
                 "본점",
                 "1시간 전",
                 CarrotMarketPrice,
-                false
+                false,
+                null
             ),
             CarrotMarketData(
                 "제품",
                 "본점",
                 "1시간 전",
                 CarrotMarketPrice,
-                false
+                false,
+                null
             ),
             CarrotMarketData(
                 "제품",
                 "본점",
                 "1시간 전",
                 CarrotMarketPrice,
-                false
+                false,
+                null
             )
         )
     )
