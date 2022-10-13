cee697350c0c038e4bead8ed2db7480868ee701d
kimjjunho <kimjjun1007@gmail.com>
2022-10-13 17:18:48 +0900

    feat:SIMT-8 성심당 근무 지점 선택 완-벽

--- feature/feature-auth/src/main/java/com/comit/feature_auth/screen/find/AuthFindScreen.kt
+++ feature/feature-auth/src/main/java/com/comit/feature_auth/screen/find/AuthFindScreen.kt
@@ -7,11 +7,13 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Stable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.comit.common.compose.noRippleClickable
 import com.comit.core_design_system.color.SimTongColor
 import com.comit.core_design_system.component.TabBar
--- feature/feature-auth/src/main/java/com/comit/feature_auth/screen/find/FindEmployeeNum.kt
+++ feature/feature-auth/src/main/java/com/comit/feature_auth/screen/find/FindEmployeeNum.kt
@@ -1,27 +1,25 @@
 package com.comit.feature_auth.screen.find
 
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.border
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.BottomSheetScaffold
-import androidx.compose.material.BottomSheetState
-import androidx.compose.material.BottomSheetValue
-import androidx.compose.material.Button
 import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.Text
-import androidx.compose.material.rememberBottomSheetScaffoldState
+import androidx.compose.material.ModalBottomSheetState
+import androidx.compose.material.ModalBottomSheetValue
+import androidx.compose.material.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.Stable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -29,39 +27,88 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusManager
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.comit.common.compose.noRippleClickable
+import com.comit.core_design_system.button.BasicButton
 import com.comit.core_design_system.button.BasicCheckBox
 import com.comit.core_design_system.button.BigRedRoundButton
 import com.comit.core_design_system.color.SimTongColor
 import com.comit.core_design_system.component.SimTongTextField
+import com.comit.core_design_system.dialog.SimBottomSheetDialog
 import com.comit.core_design_system.modifier.simSelectable
 import com.comit.core_design_system.typography.Body1
 import com.comit.core_design_system.typography.Body2
 import com.comit.core_design_system.typography.Body4
+import com.comit.core_design_system.typography.Body6
 import com.comit.core_design_system.typography.Body8
 import com.comit.feature_auth.R
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 
+private var placeName by mutableStateOf("근무 지점 선택")
+
+fun Modifier.addFocusCleaner(focusManager: FocusManager, doOnClear: () -> Unit = {}): Modifier {
+    return this.pointerInput(Unit) {
+        detectTapGestures(onTap = {
+            doOnClear()
+            focusManager.clearFocus()
+        })
+    }
+}
+
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun FindEmployeeNum(
+) {
+    val bottomSheetState = rememberModalBottomSheetState(
+        ModalBottomSheetValue.Hidden
+    )
+    val coroutineScope = rememberCoroutineScope()
 
+    SimBottomSheetDialog(
+        useHandle = true,
+        sheetState = bottomSheetState,
+        sheetContent = { FindPlaceLazyColumn() }
+    ) {
+        FindEmployeeNumBasic(
+            coroutineScope = coroutineScope,
+            bottomSheetState = bottomSheetState,
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun FindEmployeeNumBasic(
+    coroutineScope: CoroutineScope,
+    bottomSheetState: ModalBottomSheetState,
 ) {
     var name by remember { mutableStateOf<String?>(null) }
-    var workPlace by remember { mutableStateOf<String?>(null) }
     var eMail by remember { mutableStateOf<String?>(null) }
     var nameError by remember { mutableStateOf<String?>(null) }
     var workPlaceError by remember { mutableStateOf<String?>(null) }
     var emailError by remember { mutableStateOf<String?>(null) }
-    val buttonEnabled =
-        !(name == null || name == "" || workPlace == null || workPlace == "" || eMail == null || eMail == "")
+    val underButtonEnabled = !(name == null || name == ""
+            || placeName == stringResource(id = R.string.choose_work_place) || eMail == null || eMail == "")
 
     val errorMsg = stringResource(id = R.string.error_message)
+
+    val centerButtonTextColor =
+        if (placeName == stringResource(id = R.string.choose_work_place))
+            SimTongColor.Gray400 else SimTongColor.Gray900
+    val centerButtonColor =
+        if (placeName == stringResource(id = R.string.choose_work_place))
+            SimTongColor.Gray200 else SimTongColor.Gray100
+    val centerButtonBorderColor =
+        if (workPlaceError == null) SimTongColor.Gray200 else SimTongColor.Error
+
+    val localFocusManager = LocalFocusManager.current
 
     Column(
         modifier = Modifier.padding(start = 40.dp, end = 40.dp)
@@ -85,19 +132,40 @@ fun FindEmployeeNum(
 
         Spacer(modifier = Modifier.height(20.dp))
 
-        SimTongTextField(
-            value = workPlace ?: "",
-            onValueChange = {
-                workPlace = it
-                nameError = null
-                workPlaceError = null
-                emailError = null
+        BasicButton(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(44.dp)
+                .border(
+                    width = 1.dp,
+                    color = centerButtonBorderColor,
+                    shape = RoundedCornerShape(5.dp)
+                )
+                .addFocusCleaner(localFocusManager),
+            shape = RoundedCornerShape(5.dp),
+            enabled = true,
+            onClick = {
+                coroutineScope.launch {
+                    nameError = null
+                    workPlaceError = null
+                    emailError = null
+                    localFocusManager.clearFocus()
+                    bottomSheetState.show()
+                }
             },
-            hintBackgroundColor = SimTongColor.Gray200,
-            backgroundColor = SimTongColor.Gray100,
-            hint = stringResource(id = R.string.choose_work_place),
-            error = workPlaceError
-        )
+            backgroundColor = centerButtonColor,
+            pressedBackgroundColor = SimTongColor.Gray200,
+            disabledBackgroundColor = SimTongColor.Gray200,
+        ) {
+            Body6(
+                text = placeName,
+                color = centerButtonTextColor,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .wrapContentWidth(Alignment.Start)
+                    .padding(start = 14.dp)
+            )
+        }
 
         Spacer(modifier = Modifier.height(20.dp))
 
@@ -124,17 +192,118 @@ fun FindEmployeeNum(
                 nameError = ""
                 workPlaceError = ""
                 emailError = errorMsg
+                localFocusManager.clearFocus()
             },
-            enabled = buttonEnabled
+            enabled = underButtonEnabled
         )
     }
 
 }
 
-@Composable
-fun showEmployeeNum(
+data class FindPlaceDataSample(
+    val name: String,
+    val position: String
+)
 
-){
+private const val DefaultSelected: Int = -1
+
+@Composable
+fun FindPlaceLazyColumn(
+) {
+    val scrollState = rememberScrollState()
+    var selectedValue by remember { mutableStateOf(DefaultSelected) }
+
+    val isSelect: (Int) -> Boolean = { selectedValue == it }
+
+    val items =
+        (1..100).map { FindPlaceDataSample("성심당 ${it}호 점", "대전광역시 서구 계룡로 598 롯데백화점 1층") }.toList()
+
+    Column {
+        Body1(
+            text = "지점 선택",
+            modifier = Modifier
+                .padding(start = 30.dp, top = 32.dp)
+        )
+
+        Spacer(modifier = Modifier.height(17.dp))
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .verticalScroll(scrollState)
+        ) {
+            items.forEachIndexed { index, item ->
+                Box(
+                    modifier = Modifier
+                        .height(60.dp)
+                        .simSelectable(
+                            selected = isSelect(index),
+                            onClick = {
+                                selectedValue = index
+                                placeName = item.name
+                            },
+                            role = Role.RadioButton
+                        )
+                ) {
+
+                    Column(
+                        modifier = Modifier
+                            .padding(horizontal = 30.dp)
+                    ) {
+                        Spacer(modifier = Modifier.height(15.dp))
+
+                        Body4(
+                            text = item.name,
+                            color = SimTongColor.Gray900
+                        )
+
+                        Spacer(modifier = Modifier.height(3.dp))
+
+                        Body8(
+                            text = item.position,
+                            color = SimTongColor.Gray900
+                        )
+                        Canvas(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .fillMaxHeight()
+                                .wrapContentHeight(Alignment.Bottom)
+                        ) {
+                            val canvasWidth = size.width
+                            val canvasHeight = size.height
+
+                            drawLine(
+                                start = Offset(x = 0f, y = canvasHeight),
+                                end = Offset(x = canvasWidth, y = canvasHeight),
+                                color = SimTongColor.Gray900,
+                                strokeWidth = 0.1F
+                            )
+                        }
+                    }
+
+                    BasicCheckBox(
+                        checked = isSelect(index),
+                        onCheckedChange = {
+                            selectedValue = index
+                            placeName = item.name
+                        },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .wrapContentWidth(Alignment.End)
+                            .fillMaxHeight()
+                            .wrapContentHeight(Alignment.CenterVertically)
+                            .padding(end = 33.dp)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ShowEmployeeNum(
+
+) {
     Column() {
         Spacer(modifier = Modifier.height(227.5.dp))
 
@@ -174,7 +343,7 @@ fun showEmployeeNum(
     }
 }
 
-@ExperimentalMaterialApi
+/*@ExperimentalMaterialApi
 @Composable
 fun bottomSheet(
 
@@ -221,101 +390,7 @@ fun bottomSheet(
             }
         }
     }
-}
-
-data class FindPlaceDataSample(
-    val name: String,
-    val position: String
-)
-
-private const val DefaultSelected: Int = -1
-
-@Composable
-fun findPlaceLazyColumn(
-
-) {
-    val scrollState = rememberScrollState()
-    var selectedValue by remember { mutableStateOf(DefaultSelected) }
-
-    val isSelect: (Int) -> Boolean = { selectedValue == it }
-
-    val items = (1..100).map { FindPlaceDataSample("성심당 ${it}호 점", "대전광역시 서구 계룡로 598 롯데백화점 1층") }.toList()
-
-    Column {
-        Body1(
-            text = "지점 선택",
-            modifier = Modifier
-                .padding(start = 30.dp, top = 32.dp)
-        )
-
-        Spacer(modifier = Modifier.height(17.dp))
-
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .verticalScroll(scrollState)
-        ) {
-            items.forEachIndexed { index, item ->
-                Box(
-                    modifier = Modifier
-                        .height(60.dp)
-                        .simSelectable(
-                            selected = isSelect(index),
-                            onClick = { selectedValue = index },
-                            role = Role.RadioButton
-                        )
-                ) {
-
-                    Column(
-                        modifier = Modifier
-                            .padding(horizontal = 30.dp)
-                    ) {
-                        Spacer(modifier = Modifier.height(15.dp))
-
-                        Body4(
-                            text = item.name,
-                            color = SimTongColor.Gray900
-                        )
-
-                        Spacer(modifier = Modifier.height(3.dp))
-
-                        Body8(
-                            text = item.position,
-                            color = SimTongColor.Gray900
-                        )
-                        Canvas(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .fillMaxHeight()
-                                .wrapContentHeight(Alignment.Bottom)
-                        ) {
-                            val canvasWidth = size.width
-                            val canvasHeight = size.height
-
-                            drawLine(
-                                start = Offset(x = 0f, y = canvasHeight),
-                                end = Offset(x = canvasWidth, y = canvasHeight),
-                                color = SimTongColor.Gray900,
-                                strokeWidth = 0.1F
-                            )
-                        }
-                    }
-
-                    BasicCheckBox(
-                        checked = isSelect(index),
-                        onCheckedChange = { selectedValue = index },
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .wrapContentWidth(Alignment.End)
-                            .fillMaxHeight()
-                            .wrapContentHeight(Alignment.CenterVertically)
-                            .padding(end = 33.dp)
-                    )
-                }
-            }
-        }
-    }
-}
+}*/
 
 @Preview(showBackground = true)
 @Composable
@@ -325,19 +400,6 @@ fun PreviewFindEmployeeNumScreen() {
 
 @Preview
 @Composable
-fun PreviewShowEmployeeScreen(){
-    showEmployeeNum()
-}
-
-@ExperimentalMaterialApi
-@Preview(showBackground = true)
-@Composable
-fun PreviewBottomSheet() {
-    bottomSheet()
-}
-
-@Preview
-@Composable
-fun PreviewFindPlaceLazyColumn() {
-    findPlaceLazyColumn()
+fun PreviewShowEmployeeScreen() {
+    ShowEmployeeNum()
 }
