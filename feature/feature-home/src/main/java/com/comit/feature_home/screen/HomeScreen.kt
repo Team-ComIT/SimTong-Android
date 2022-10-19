9881f50106ff0d007dbb3a6044ab4876d3310aed
kimjjunho <kimjjun1007@gmail.com>
2022-10-19 23:14:46 +0900

    feat: SIMT-26 홈 화면 급여정보, 휴무표 작성 ui

--- feature/feature-home/src/main/java/com/comit/feature_home/screen/HomeScreen.kt
+++ feature/feature-home/src/main/java/com/comit/feature_home/screen/HomeScreen.kt
@@ -1,31 +1,42 @@
 package com.comit.feature_home.screen
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Card
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.comit.core_design_system.color.SimTongColor
 import com.comit.core_design_system.component.FoodList
 import com.comit.core_design_system.component.Header
 import com.comit.core_design_system.icon.SimTongIcon
+import com.comit.core_design_system.modifier.simClickable
 import com.comit.core_design_system.theme.SimTongTheme
+import com.comit.core_design_system.typography.Body14
+import com.comit.core_design_system.typography.Body5
 import com.comit.core_design_system.typography.Title3
 import com.comit.core_design_system.util.currentMealsTime
 import com.example.feature_home.R
@@ -102,11 +113,83 @@ fun HomeScreen() {
             )
 
             Spacer(modifier = Modifier.height(27.dp))
+            
+            HomeUnderRowItem(
+                painter = painterResource(id = R.drawable.ic_home_coin),
+                title = "나의 급여 정보",
+                content = "이번 달 나의 급여 정보를 손쉽게 확인해보세요."
+            )
 
+            Spacer(modifier = Modifier.height(16.dp))
+            
+            HomeUnderRowItem(
+                painter = painterResource(id = R.drawable.ic_home_schedule),
+                title = "휴무표 작성",
+                content = "이번 달 나의 휴무표를 직접 작성해 일정을 손쉽게 관리해 보세요."
+            )
 
+            Spacer(modifier = Modifier.height(46.dp))
         }
     }
 }
+
+@Composable
+fun HomeUnderRowItem(
+    painter: Painter,
+    title: String,
+    content: String,
+    onClick: () -> Unit = {}
+){
+    Card(
+        shape = RoundedCornerShape(4.dp),
+        elevation = 2.dp,
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(48.dp)
+            .simClickable { onClick() }
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight()
+                .background(
+                    color = SimTongColor.Transparent,
+                    shape = RoundedCornerShape(4.dp)
+                )
+        ) {
+
+            Spacer(modifier = Modifier.width(12.dp))
+
+            Image(
+                painter = painter,
+                contentDescription = null,
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .wrapContentHeight(Alignment.CenterVertically)
+                    .size(28.dp)
+            )
+
+            Spacer(modifier = Modifier.width(11.dp))
+
+            Column(
+                modifier = Modifier
+                    .height(48.dp)
+                    .fillMaxHeight()
+                    .wrapContentHeight(Alignment.CenterVertically)
+            ) {
+                Body5(
+                    text = title,
+                    color = SimTongColor.Gray900
+                )
+                Body14(
+                    text = content,
+                    color = SimTongColor.Gray400
+                )
+            }
+        }
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
