1cc55eb364bfc61b0eddd88af40f92dfdf8a9a72
limsaehyun <sh00710000@gmail.com>
2022-11-03 22:17:46 +0900

    feat: SIMT-36 time util

--- common/src/main/java/com/comit/common/domain/unit/Time.kt
+++ common/src/main/java/com/comit/common/domain/unit/Time.kt
@@ -7,3 +7,27 @@ import java.util.Date
 @SuppressLint("SimpleDateFormat")
 val currentTime =
     SimpleDateFormat("HHmm").format(Date(System.currentTimeMillis())).toInt()
+
+private const val OneMinuteForSecond: Int = 60
+
+private const val CheckDigit: Int = 10
+
+fun getMinute(minute: Int): Int =
+    minute / OneMinuteForSecond
+
+fun getSecond(minute: Int): Int =
+    if (CheckDigit <= minute % OneMinuteForSecond) minute % OneMinuteForSecond
+    else minute % OneMinuteForSecond
+
+fun getStringTime(minute: Int, second: Int): String {
+    val str = StringBuilder()
+
+    str
+        .append("남은 시간")
+        .append(" ")
+        .append(minute)
+        .append(" : ")
+        .append(second)
+
+    return str.toString()
+}
