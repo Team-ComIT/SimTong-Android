c15ae12643b17da686d651acf0543d4b83134c61
kimjjunho <kimjjun1007@gmail.com>
2022-11-11 11:08:11 +0900

    feat: SIMT-38 해당 달 리스트 뽑아오기

--- feature/feature-home/src/main/java/com/comit/feature_home/screen/SimTongCalendar.kt
+++ feature/feature-home/src/main/java/com/comit/feature_home/screen/SimTongCalendar.kt
@@ -1,11 +1,13 @@
 package com.comit.feature_home.screen
 
+import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -28,7 +30,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import com.comit.common.compose.noRippleClickable
 import com.comit.core_design_system.color.SimTongColor
 import com.comit.core_design_system.icon.SimTongIcon
 import com.comit.core_design_system.typography.Body11
@@ -37,12 +41,23 @@ import com.comit.core_design_system.typography.Body13
 import com.comit.core_design_system.typography.Body3
 import com.comit.core_design_system.typography.Body6
 import com.example.feature_home.R
+import java.text.DecimalFormat
+import java.time.LocalDate
+import java.time.Month
+import java.util.Calendar
 import java.util.GregorianCalendar
 
-
+data class SimTongCalendarData(
+    val day: String,
+    val workCount: Int,
+    val weekend: Boolean,
+    val thisMouth: Boolean,
+    val restDay: Boolean,
+    val annualDay: Boolean
+)
 
 @Stable
-private fun Modifier.simTongCalendarTotalHeight() = height(420.dp)
+private val SimTongCalendarTotalHeight: Dp = 420.dp
 
 @Stable
 private val SimTongCalendarTotalRound = RoundedCornerShape(20.dp)
@@ -56,48 +71,69 @@ private val SimTongCalendarTotalHorizontalPadding = PaddingValues(
 fun SimTongCalendar(
 
 ) {
-
-    var year by remember { mutableStateOf("") }
-    var month by remember { mutableStateOf("") }
-    var day by remember { mutableStateOf("") }
+    var checkMonth by remember { mutableStateOf(0) }
 
     val today = GregorianCalendar()
+    var calendar = GregorianCalendar(today.get(Calendar.YEAR),today.get(Calendar.MONTH),1)
 
+    var year by remember { mutableStateOf(calendar.get(Calendar.YEAR)) }
+    var month by remember { mutableStateOf(calendar.get(Calendar.MONTH)+1) }
+    var day by remember { mutableStateOf(calendar.get(Calendar.DATE)) }
 
+    var calendarList by remember { mutableStateOf(organizeList(1)) }
 
     Column(
         modifier = Modifier
+            .padding(SimTongCalendarTotalHorizontalPadding)
             .fillMaxWidth()
-            .simTongCalendarTotalHeight()
+            .height(SimTongCalendarTotalHeight)
             .background(
                 color = SimTongColor.Gray50,
                 shape = SimTongCalendarTotalRound
             )
-            .padding(SimTongCalendarTotalHorizontalPadding)
     ) {
 
         Spacer(modifier = Modifier.height(15.dp))
 
         SimTongCalendarDate(
-            year = year,
-            month = month,
-            day = day,
+            year = year.toString(),
+            month = month.toString(),
+            day = day.toString(),
             onBeforeClicked = {
+                checkMonth --
+                calendar = GregorianCalendar(today.get(Calendar.YEAR),today.get(Calendar.MONTH)+checkMonth,1)
+                year = calendar.get(Calendar.YEAR)
+                month = calendar.get(Calendar.MONTH) + 1
             },
-            onNextClicked = {}
+            onNextClicked = {
+                checkMonth ++
+                calendar = GregorianCalendar(today.get(Calendar.YEAR),today.get(Calendar.MONTH)+checkMonth,1)
+                year = calendar.get(Calendar.YEAR)
+                month = calendar.get(Calendar.MONTH) + 1
+            }
         )
         
         Spacer(modifier = Modifier.height(37.dp))
 
-        WeekTopRow()
+        Column(
+            modifier = Modifier
+                .padding(horizontal = 14.dp)
+                .fillMaxWidth()
+        ) {
+            WeekTopRow()
+
+            SimTongCalendarList(
+                list = calendarList
+            ) { day = returnDay(day.toString()) }
+        }
     }
 }
 
 @Stable
-private fun Modifier.simTongCalendarDateHeight() = height(30.dp)
+private val SimTongCalendarDateHeight: Dp = 30.dp
 
 @Stable
-private fun Modifier.simTongCalendarDateButtonSize() = size(20.dp)
+private val SimTongCalendarDateButtonSize:Dp = 20.dp
 
 @Composable
 fun SimTongCalendarDate(
@@ -114,9 +150,9 @@ fun SimTongCalendarDate(
     Row(
         verticalAlignment = Alignment.CenterVertically,
         modifier = Modifier
-            .fillMaxWidth()
-            .simTongCalendarDateHeight()
             .padding(start = 16.dp, end = 20.dp)
+            .fillMaxWidth()
+            .height(SimTongCalendarDateHeight)
     ) {
         Body3(
             text = "$year $month $day",
@@ -132,7 +168,7 @@ fun SimTongCalendarDate(
             IconButton(
                 onClick = { onBeforeClicked() },
                 modifier = Modifier
-                    .simTongCalendarDateButtonSize()
+                    .size(SimTongCalendarDateButtonSize)
             ) {
                 Icon(
                     painter = painterResource(id = SimTongIcon.Calendar_Before.drawableId),
@@ -145,7 +181,7 @@ fun SimTongCalendarDate(
             IconButton(
                 onClick = { onNextClicked() },
                 modifier = Modifier
-                    .simTongCalendarDateButtonSize()
+                    .size(SimTongCalendarDateButtonSize)
             ) {
                 Icon(
                     painter = painterResource(id = SimTongIcon.Calendar_After.drawableId),
@@ -168,11 +204,10 @@ fun WeekTopRow(){
     val saturday: String = stringResource(id = R.string.calendar_saturday)
 
     LazyRow(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(horizontal = 14.dp)
+        modifier = Modifier.fillMaxWidth()
     ){
         val list = listOf(sunday,monday,tuesday,wednesday,thursday,friday,saturday)
+
         items(list){
             val textColor =
                 if(it == stringResource(id = R.string.calendar_sunday)
@@ -192,28 +227,87 @@ fun WeekTopRow(){
     }
 }
 
-@Stable
-private fun Modifier.simTongCalendarItemSize() = size(40.dp)
+@Composable
+fun SimTongCalendarList(
+    list: List<SimTongCalendarData>,
+    onItemClicked: (String) -> Unit
+){
+
+
+//    Column(
+//        modifier = Modifier
+//            .fillMaxWidth()
+//    ) {
+//        repeat(list.size/7){
+//
+//        }
+//    }
+//    LazyRow(
+//        modifier = Modifier
+//            .fillMaxWidth()
+//    ) {
+//        items(list){
+//            Log.d("TAG", "SimTongCalendarList: $date")
+//
+//            SimTongCalendarItem(
+//                day = list[date].day,
+//                workCount = list[date].workCount,
+//                weekend = list[date].weekend,
+//                thisMouth = list[date].thisMouth,
+//                restDay = list[date].restDay,
+//                annualDay = list[date].annualDay,
+//                onItemClicked = onItemClicked,
+//                modifier = Modifier
+//                    .fillParentMaxSize(1.toFloat()/7.toFloat())
+//            )
+//
+//            date++
+//        }
+//    }
+    LazyRow(
+        modifier = Modifier
+            .fillMaxWidth()
+    ) {
+        items(list){
+            SimTongCalendarItem(
+                day = it.day,
+                workCount = it.workCount,
+                weekend = it.weekend,
+                thisMouth = it.thisMouth,
+                restDay = it.restDay,
+                annualDay = it.annualDay,
+                onItemClicked = onItemClicked,
+                modifier = Modifier
+                    .fillParentMaxSize(1.toFloat()/7.toFloat())
+            )
+        }
+    }
+
+}
 
 @Stable
-private fun Modifier.simTongCalendarItemBoxSize() = size(23.64.dp)
+private val SimTongCalendarItemSize: Dp = 40.dp
+
+@Stable
+private val SimTongCalendarItemBoxSize: Dp = 23.64.dp
 
 @Stable
 private val SimTongCalendarItemBoxRound = RoundedCornerShape(9.09.dp)
 
 @Composable
 fun SimTongCalendarItem(
+    modifier: Modifier = Modifier,
     day: String,
     workCount: Int,
     weekend: Boolean,
     thisMouth: Boolean,
     restDay: Boolean,
     annualDay: Boolean,
-    onItemClicked: (() -> Unit)? = null
+    onItemClicked: (String) -> Unit
 ){
     val textColor =
-        if(thisMouth) SimTongColor.Gray800
-        else if (weekend) SimTongColor.Gray300
+        if(weekend) SimTongColor.Gray300
+        else if (thisMouth) SimTongColor.Gray800
         else SimTongColor.Gray100
 
     val backgroundColor =
@@ -223,9 +317,7 @@ fun SimTongCalendarItem(
 
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier
-            .simTongCalendarItemSize()
-            .background(color = SimTongColor.Gray50)
+        modifier = modifier.noRippleClickable { onItemClicked(day) }
     ){
         Body12(
             text = day,
@@ -235,7 +327,7 @@ fun SimTongCalendarItem(
         Box(
             contentAlignment = Alignment.Center,
             modifier = Modifier
-                .simTongCalendarItemBoxSize()
+                .size(SimTongCalendarItemBoxSize)
                 .background(
                     color = backgroundColor,
                     shape = SimTongCalendarItemBoxRound
@@ -251,7 +343,7 @@ fun SimTongCalendarItem(
                     text = stringResource(id = R.string.calendar_annual_day),
                     color = SimTongColor.White
                 )
-            }else {
+            }else if(thisMouth){
                 Body11(
                     text = workCount.toString(),
                     color = SimTongColor.Gray400
@@ -261,8 +353,84 @@ fun SimTongCalendarItem(
     }
 }
 
+private fun returnDay(day: String): Int = day.toInt()
+
+private fun organizeList(
+    checkMonth: Int
+): ArrayList<SimTongCalendarData> {
+    val calendarList = ArrayList<SimTongCalendarData>()
+    val today = GregorianCalendar()
+    val calendar = GregorianCalendar(today.get(Calendar.YEAR),today.get(Calendar.MONTH)+checkMonth,1)
+    val min = calendar.get(Calendar.DAY_OF_WEEK)-1
+    val max = calendar.getActualMaximum(Calendar.DAY_OF_MONTH)
+    val year = calendar.get(Calendar.YEAR)
+    val mouth = calendar.get(Calendar.MONTH)+1
+
+    val lastCalendar = GregorianCalendar(today.get(Calendar.YEAR),today.get(Calendar.MONTH)+checkMonth-1,1)
+    val lastMax = lastCalendar.getActualMaximum(Calendar.DAY_OF_MONTH)
+
+    for(i in min-1 downTo  0){
+        calendarList.add(
+            SimTongCalendarData(
+                day = (lastMax-i).toString(),
+                workCount = 0,
+                weekend = false,
+                thisMouth = false,
+                restDay = false,
+                annualDay = false
+            )
+        )
+
+        Log.d("TAG", "before: "+(lastMax-i))
+    }
+
+    for (i in 1..max){
+
+        val dayOfWeek = LocalDate.of(year,mouth,i).dayOfWeek.value
+        val weekend = dayOfWeek == 6 || dayOfWeek == 7
+
+        calendarList.add(
+            SimTongCalendarData(
+                day = i.toString(),
+                workCount = 0,
+                weekend = weekend,
+                thisMouth = true,
+                restDay = false,
+                annualDay = false
+            )
+        )
+
+        Log.d("TAG", "this: $i")
+    }
+
+    for(i in 1..7){
+        if(calendarList.size%7 == 0){
+            Log.d("TAG", "break: ")
+            break
+        } else {
+            calendarList.add(
+                SimTongCalendarData(
+                    day = i.toString(),
+                    workCount = 0,
+                    weekend = false,
+                    thisMouth = false,
+                    restDay = false,
+                    annualDay = false
+                )
+            )
+            Log.d("TAG", "next: $i")
+        }
+    }
+
+    return calendarList
+}
+
+
+
 @Preview(showBackground = true)
 @Composable
 fun ShowCalendar(){
-    SimTongCalendar()
+    Column(modifier = Modifier.fillMaxSize()) {
+        SimTongCalendar()
+    }
 }
