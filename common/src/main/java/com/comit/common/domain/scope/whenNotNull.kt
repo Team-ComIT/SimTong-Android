a4b3798e935213c39663ba5fb7c39fda5792ce48
limsaehyun <sh00710000@gmail.com>
2022-11-21 11:36:00 +0900

    feat: SIMT-19 whenNotNull

---
+++ common/src/main/java/com/comit/common/domain/scope/whenNotNull.kt
@@ -0,0 +1,13 @@
+package com.comit.common.domain.scope
+
+fun <T: Any, R: Any> whenAllNotNull(vararg options: T?, block: (List<T>)->R) {
+    if (options.all { it != null }) {
+        block(options.filterNotNull())
+    }
+}
+
+fun <T: Any, R: Any> whenAnyNotNull(vararg options: T?, block: (List<T>)->R) {
+    if (options.any { it != null }) {
+        block(options.filterNotNull())
+    }
+}
