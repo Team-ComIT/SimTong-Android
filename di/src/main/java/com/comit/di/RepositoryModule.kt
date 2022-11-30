73f12cfb7e49d0a9d3f02bd6c63f417ec088ae4a
limsaehyun <sh00710000@gmail.com>
2022-11-30 20:26:02 +0900

    feat: SIMT-19 provideAuthRepository

---
+++ di/src/main/java/com/comit/di/RepositoryModule.kt
@@ -0,0 +1,20 @@
+package com.comit.di
+
+import com.comit.data.repository.AuthRepositoryImpl
+import com.comit.domain.repository.AuthRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+
+    @Singleton
+    @Binds
+    abstract fun provideAuthRepository(
+        authRepositoryImpl: AuthRepositoryImpl
+    ): AuthRepository
+}
