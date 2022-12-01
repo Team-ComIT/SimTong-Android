8016a44f40892a1ff0a5735ab67e9b5c28a08510
limsaehyun <sh00710000@gmail.com>
2022-12-01 21:03:07 +0900

    feat: SIMT-41 VerificationEmployeeUseCase

---
+++ domain/src/main/java/com/comit/domain/usecase/VerificationEmployeeUseCase.kt
@@ -0,0 +1,23 @@
+package com.comit.domain.usecase
+
+import com.comit.domain.repository.AuthRepository
+import javax.inject.Inject
+
+class VerificationEmployeeUseCase @Inject constructor(
+    private val repository: AuthRepository,
+) {
+
+    suspend operator fun invoke(
+        params: Params
+    ) = kotlin.runCatching {
+        repository.verificationEmployee(
+            name = params.name,
+            employeeNumber = params.employeeNumber,
+        )
+    }
+
+    data class Params(
+        val name: String,
+        val employeeNumber: String,
+    )
+}
