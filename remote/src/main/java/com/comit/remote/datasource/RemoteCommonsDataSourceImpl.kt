60ac749a212b9427fd586a6d508c999a8f56973b
limsaehyun <sh00710000@gmail.com>
2022-12-01 23:21:59 +0900

    feat: SIMT-42 RemoteCommonsDataSource & Impl

---
+++ remote/src/main/java/com/comit/remote/datasource/RemoteCommonsDataSourceImpl.kt
@@ -0,0 +1,76 @@
+package com.comit.remote.datasource
+
+import com.comit.data.datasource.RemoteCommonsDataSource
+import com.comit.model.SpotList
+import com.comit.model.Token
+import com.comit.remote.api.CommonsAPI
+import com.comit.remote.mapper.toModel
+import com.comit.remote.request.commons.InitializationPasswordRequest
+import javax.inject.Inject
+
+class RemoteCommonsDataSourceImpl @Inject constructor(
+    private val commonsAPI: CommonsAPI,
+): RemoteCommonsDataSource {
+
+    override suspend fun findEmployeeNumber(
+        name: String,
+        spotId: String,
+        email: String,
+    ): String {
+        return commonsAPI.findEmployeeNumber(
+            name = name,
+            spotId = spotId,
+            email = email,
+        ).employeeNumber
+    }
+
+    override suspend fun tokenReissue(): Token {
+        return commonsAPI.tokenReissue().toModel()
+    }
+
+    override suspend fun findAccountExist(
+        employeeNumber: Int,
+        email: String,
+    ) {
+        commonsAPI.findAccountExist(
+            employeeNumber = employeeNumber,
+            email = email,
+        )
+    }
+
+    override suspend fun findEmailDuplication(
+        email: String,
+    ) {
+        commonsAPI.findEmailDuplication(
+            email = email,
+        )
+    }
+
+    override suspend fun changePassword(
+        password: String,
+        newPassword: String,
+    ) {
+        commonsAPI.changePassword(
+            password = password,
+            newPassword = newPassword,
+        )
+    }
+
+    override suspend fun fetchSpots(): SpotList {
+        return commonsAPI.fetchSpots().toModel()
+    }
+
+    override suspend fun initializationPassword(
+        email: String,
+        employeeNumber: Int,
+        newPassword: String,
+    ) {
+        commonsAPI.initializationPassword(
+            request = InitializationPasswordRequest(
+                email = email,
+                employeeNumber = employeeNumber,
+                newPassword = newPassword
+            )
+        )
+    }
+}
