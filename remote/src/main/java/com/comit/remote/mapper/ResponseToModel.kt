b2d56d77560b820e17b5c0db77e0317372f13aaa
limsaehyun <sh00710000@gmail.com>
2022-12-01 23:23:28 +0900

    feat: SIMT-42 Commons ReponseToModel Mapper

--- remote/src/main/java/com/comit/remote/mapper/ResponseToModel.kt
+++ remote/src/main/java/com/comit/remote/mapper/ResponseToModel.kt
@@ -1,10 +1,14 @@
 package com.comit.remote.mapper
 
+import com.comit.model.SpotList
 import com.comit.model.Token
 import com.comit.model.User
+import com.comit.remote.response.commons.FetchSpotsResponse
+import com.comit.remote.response.commons.ReissueTokenResponse
 import com.comit.remote.response.users.FetchUserInformationResponse
 import com.comit.remote.response.users.SignInResponse
 import com.comit.remote.response.users.SignUpResponse
+import kotlin.math.log
 
 internal fun SignInResponse.toModel() =
     Token(
@@ -27,4 +31,24 @@ internal fun FetchUserInformationResponse.toModel() =
         nickname = nickname,
         spot = spot,
         profileImagePath = profileImagePath,
+    )
+
+internal fun FetchSpotsResponse.toModel(): SpotList {
+    fun FetchSpotsResponse.Spot.toModel() =
+        SpotList.Spot(
+            id = id,
+            name = name,
+            location = location,
+        )
+
+    return SpotList(
+        spotList = spotList.map { it.toModel() }
+    )
+}
+
+internal fun ReissueTokenResponse.toModel() =
+    Token(
+        accessToken = accessToken,
+        accessTokenExp = accessTokenExp,
+        refreshToken = refreshToken,
     )
