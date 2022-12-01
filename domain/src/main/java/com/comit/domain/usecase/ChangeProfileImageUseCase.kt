2b4950fd38873638318219814912defcaf9a2460
limsaehyun <sh00710000@gmail.com>
2022-12-01 21:02:14 +0900

    feat: SIMT-41 ChangeProfileImageUseCase

---
+++ domain/src/main/java/com/comit/domain/usecase/ChangeProfileImageUseCase.kt
@@ -0,0 +1,21 @@
+package com.comit.domain.usecase
+
+import com.comit.domain.repository.AuthRepository
+import javax.inject.Inject
+
+class ChangeProfileImageUseCase @Inject constructor(
+    private val repository: AuthRepository,
+) {
+
+    suspend operator fun invoke(
+        params: Params,
+    ) = kotlin.runCatching {
+        repository.changeProfileImage(
+            profileImagePath = params.profileImagePath,
+        )
+    }
+
+    data class Params(
+        val profileImagePath: String,
+    )
+}
