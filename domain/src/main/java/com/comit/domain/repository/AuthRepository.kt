d0b59d986aa5192fc35c88e772acb8ff8ebf8730
limsaehyun <sh00710000@gmail.com>
2022-12-01 21:00:23 +0900

    feat: SIMT-41 AuthRepository & Impl

--- domain/src/main/java/com/comit/domain/repository/AuthRepository.kt
+++ domain/src/main/java/com/comit/domain/repository/AuthRepository.kt
@@ -1,6 +1,8 @@
 package com.comit.domain.repository
 
 import com.comit.model.Token
+import com.comit.model.User
+import java.util.*
 
 interface AuthRepository {
 
@@ -8,4 +10,40 @@ interface AuthRepository {
         employeeNumber: Int,
         password: String,
     ): Token
+
+    suspend fun verificationEmployee(
+        name: String,
+        employeeNumber: String,
+    )
+
+    suspend fun signUp(
+        name: String,
+        employeeNumber: Int,
+        email: String,
+        password: String,
+        nickname: String?,
+        profileImagePath: String?,
+    ): Token
+
+    suspend fun checkNicknameDuplication(
+        nickname: String,
+    )
+
+    suspend fun changeSpot(
+        spotId: UUID,
+    )
+
+    suspend fun changeProfileImage(
+        profileImagePath: String,
+    )
+
+    suspend fun changeEmail(
+        email: String,
+    )
+
+    suspend fun changeNickname(
+        nickname: String,
+    )
+
+    suspend fun fetchUserInformation(): User
 }
