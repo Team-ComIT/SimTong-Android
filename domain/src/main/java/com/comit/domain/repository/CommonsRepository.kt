1d3fdd928d2105823a7756eb3d59dd1c2fbd5e8a
limsaehyun <sh00710000@gmail.com>
2022-12-01 23:22:21 +0900

    feat: SIMT-42 CommonsRepository & Impl

---
+++ domain/src/main/java/com/comit/domain/repository/CommonsRepository.kt
@@ -0,0 +1,37 @@
+package com.comit.domain.repository
+
+import com.comit.model.SpotList
+import com.comit.model.Token
+
+interface CommonsRepository {
+
+    suspend fun findEmployeeNumber(
+        name: String,
+        spotId: String,
+        email: String,
+    ): String
+
+    suspend fun tokenReissue()
+
+    suspend fun findAccountExist(
+        employeeNumber: Int,
+        email: String
+    )
+
+    suspend fun findEmailDuplication(
+        email: String
+    )
+
+    suspend fun changePassword(
+        password: String,
+        newPassword: String,
+    )
+
+    suspend fun fetchSpots(): SpotList
+
+    suspend fun initializationPassword(
+        email: String,
+        employeeNumber: Int,
+        newPassword: String,
+    )
+}
