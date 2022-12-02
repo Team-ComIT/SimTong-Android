fe52531fa23b0037e324824a09bc82003132fa5e
limsaehyun <sh00710000@gmail.com>
2022-12-02 10:47:34 +0900

    feat: SIMT-46 ChangePersonalScheduleUseCase

---
+++ domain/src/main/java/com/comit/domain/usecase/schedule/ChangePersonalScheduleUseCase.kt
@@ -0,0 +1,31 @@
+package com.comit.domain.usecase.schedule
+
+import com.comit.domain.repository.ScheduleRepository
+import java.sql.Time
+import java.util.UUID
+import javax.inject.Inject
+
+class ChangePersonalScheduleUseCase @Inject constructor(
+    private val repository: ScheduleRepository,
+) {
+
+    suspend operator fun invoke(
+        params: Params,
+    ) = kotlin.runCatching {
+        repository.changePersonalSchedule(
+            scheduleId = params.scheduleId,
+            title = params.title,
+            startAt = params.startAt,
+            endAt = params.endAt,
+            alarm = params.alarms,
+        )
+    }
+
+    data class Params(
+        val scheduleId: UUID,
+        val title: String,
+        val startAt: String,
+        val endAt: String,
+        val alarms: Time?,
+    )
+}
