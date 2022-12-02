61d88ff8819aca73d842ff5a946811077272a6f3
limsaehyun <sh00710000@gmail.com>
2022-12-02 10:09:09 +0900

    feat: SIMT-45 메뉴 관련 di 설정

--- di/src/main/java/com/comit/di/RepositoryModule.kt
+++ di/src/main/java/com/comit/di/RepositoryModule.kt
@@ -4,8 +4,10 @@ package com.comit.di
 
 import com.comit.data.repository.AuthRepositoryImpl
 import com.comit.data.repository.CommonsRepositoryImpl
+import com.comit.data.repository.MenuRepositoryImpl
 import com.comit.domain.repository.AuthRepository
 import com.comit.domain.repository.CommonsRepository
+import com.comit.domain.repository.MenuRepository
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -27,4 +29,10 @@ abstract class RepositoryModule {
     abstract fun provideCommonsRepository(
         commonsRepositoryImpl: CommonsRepositoryImpl,
     ): CommonsRepository
+
+    @Singleton
+    @Binds
+    abstract fun provideMenuRepository(
+        menuRepositoryImpl: MenuRepositoryImpl,
+    ): MenuRepository
 }
