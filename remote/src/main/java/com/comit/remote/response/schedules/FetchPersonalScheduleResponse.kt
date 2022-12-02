b3153761c964b0ccc0df1879a8211a21dd65a515
limsaehyun <sh00710000@gmail.com>
2022-12-02 10:45:52 +0900

    fix: SIMT-46 미사용 DTO 삭제

--- remote/src/main/java/com/comit/remote/response/schedules/FetchPersonalScheduleResponse.kt
+++ remote/src/main/java/com/comit/remote/response/schedules/FetchPersonalScheduleResponse.kt
@@ -2,17 +2,23 @@ package com.comit.remote.response.schedules
 
 import com.google.gson.annotations.SerializedName
 
-data class SimpleSchedulesResponse(
+data class FetchPersonalScheduleResponse(
 
     @field:SerializedName("schedules")
-    val schedules: List<Schedules>,
+    val schedules: List<Schedule>,
 ) {
-    data class Schedules(
+    data class Schedule(
+
+        @field:SerializedName("id")
+        val id: String,
 
         @field:SerializedName("start_at")
         val startAt: String,
 
         @field:SerializedName("end_at")
         val endAt: String,
+
+        @field:SerializedName("title")
+        val title: String,
     )
 }
