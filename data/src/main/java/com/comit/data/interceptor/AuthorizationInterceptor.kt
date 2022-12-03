88d412eef8c0a1c9b34f227ec40041652c03cebf
limsaehyun <sh00710000@gmail.com>
2022-12-03 23:38:28 +0900

    feat: SIMT-48 AuthoirzationInterceptor
    * 구현하면서 SIMT-50, SIMT-49 이슈 발생

---
+++ data/src/main/java/com/comit/data/interceptor/AuthorizationInterceptor.kt
@@ -0,0 +1,102 @@
+package com.comit.data.interceptor
+
+import com.comit.data.datasource.LocalAuthDataSource
+import com.comit.data.datasource.RemoteCommonsDataSource
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
+import okhttp3.Interceptor
+import okhttp3.Response
+import java.time.LocalDateTime
+import java.time.ZoneId
+import javax.inject.Inject
+
+private data class CustomRequest(
+    val path: String,
+    val method: CustomRestMethod,
+)
+
+// TODO ("모든 Method를 포함하는 ALL 을 만들기 - SIMT-50")
+internal enum class CustomRestMethod {
+    GET,
+    POST,
+    PUT,
+    PATCH,
+    DELETE,
+}
+
+internal fun String.toCustomRestMethod() =
+    when (this) {
+        "GET" -> CustomRestMethod.GET
+        "POST" -> CustomRestMethod.POST
+        "PUT" -> CustomRestMethod.PUT
+        "PATCH" -> CustomRestMethod.PATCH
+        "DELETE" -> CustomRestMethod.DELETE
+        else -> CustomRestMethod.GET
+    }
+
+/**
+ * authorization interceptor ignore request list
+ */
+private val ignoreRequest = listOf(
+    CustomRequest("/users/tokens", CustomRestMethod.POST),
+    CustomRequest("/users/verification-employee", CustomRestMethod.GET),
+    CustomRequest("/users", CustomRestMethod.POST),
+    CustomRequest("/users/nickname/duplication", CustomRestMethod.GET),
+)
+
+class AuthorizationInterceptor @Inject constructor(
+    private val localAuthDataSource: LocalAuthDataSource,
+    private val remoteCommonsDataSource: RemoteCommonsDataSource,
+) : Interceptor {
+
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val request = chain.request()
+        val path = request.url().encodedPath()
+        val method = request.method().toCustomRestMethod()
+
+        if (ignoreRequest.contains(CustomRequest(path, method))) {
+            return chain.proceed(request)
+        }
+
+        val expiredAt = runBlocking {
+            localAuthDataSource.fetchExpiredAt().first()
+        }
+
+        val currentTime = LocalDateTime.now(ZoneId.systemDefault())
+
+        if (expiredAt.isBefore(currentTime)) {
+            val refreshToken = runBlocking {
+                localAuthDataSource.fetchRefreshToken().first()
+            }
+
+            val response = runBlocking {
+                remoteCommonsDataSource.tokenReissue(
+                    refreshToken = refreshToken,
+                )
+            }
+
+            runBlocking {
+                localAuthDataSource.apply {
+                    saveAccessToken(response.accessToken)
+                    saveRefreshToken(response.refreshToken)
+                    saveExpiredAt(LocalDateTime.parse(response.accessTokenExp))
+                }
+            }
+        }
+
+        val accessToken = runBlocking { localAuthDataSource.fetchAccessToken() }
+
+        return chain.proceed(
+            request.newBuilder().addHeader(
+                AUTHORIZATION,
+                "$BEARER_HEADER $accessToken",
+            ).build()
+        )
+    }
+
+    companion object {
+        // TODO ("좀 더 잘 처리하는 방법 고민하기 - SIMT-49")
+        const val BEARER_HEADER = "Bearer"
+        const val AUTHORIZATION = "Authorization"
+    }
+}
