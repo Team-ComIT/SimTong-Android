075469add48bf32949ac792d8a6bcaf89e41ea40
limsaehyun <sh00710000@gmail.com>
2022-12-06 21:54:40 +0900

    feat: SIMT-56 Token 정보를 한 번에 저장할 수 있는 메서드

--- local/src/main/java/com/comit/local/datasource/LocalAuthDataSourceImpl.kt
+++ local/src/main/java/com/comit/local/datasource/LocalAuthDataSourceImpl.kt
@@ -2,11 +2,17 @@ package com.comit.local.datasource
 
 import com.comit.data.datasource.LocalAuthDataSource
 import com.comit.local.preference.AuthPreference
+import com.comit.model.Token
 import kotlinx.coroutines.flow.Flow
+import java.time.LocalDate
 import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.time.format.DateTimeFormatterBuilder
+import java.time.format.DateTimeParseException
+import java.time.temporal.ChronoField
 import javax.inject.Inject
 
-data class LocalAuthDataSourceImpl @Inject constructor(
+class LocalAuthDataSourceImpl @Inject constructor(
     private val authPreference: AuthPreference,
 ) : LocalAuthDataSource {
 
@@ -38,5 +44,11 @@ data class LocalAuthDataSourceImpl @Inject constructor(
         authPreference.saveExpiredAt(
             expiredAt = expiredAt,
         )
+    }
+
+    override suspend fun saveToken(token: Token) {
+        saveAccessToken(token.accessToken)
+        saveRefreshToken(token.refreshToken)
+        saveExpiredAt(token.accessTokenExp)
     }
 }
