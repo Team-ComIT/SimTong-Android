45c03cdf232b7ce1235fe07546a9528ce8e55682
limsaehyun <sh00710000@gmail.com>
2022-12-06 11:27:23 +0900

    fix: SIMT-56 SimTypography Argument로 TextAlign 추가

--- feature/feature-auth/src/main/java/com/comit/feature_auth/screen/find/findEmployeeNumber/FindEmployeeNumScreen.kt
+++ feature/feature-auth/src/main/java/com/comit/feature_auth/screen/find/findEmployeeNumber/FindEmployeeNumScreen.kt
@@ -27,12 +27,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -332,7 +332,8 @@ fun FindEmployeeNumResultScreen(
         Body4(
             text = "$name 회원님이\n요청하는 회원 정보입니다.",
             color = SimTongColor.Gray800,
-            modifier = Modifier.align(CenterHorizontally),
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            textAlign = TextAlign.Center,
         )
 
         Spacer(modifier = Modifier.height(5.dp))
