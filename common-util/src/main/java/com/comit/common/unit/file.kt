df60a30618e697d840dad1ae45627cf7ecd11262
limsaehyun <sh00710000@gmail.com>
2022-12-10 22:30:51 +0900

    feat: SIMT-76 file size util

---
+++ common-util/src/main/java/com/comit/common/unit/file.kt
@@ -0,0 +1,9 @@
+package com.comit.common.unit
+
+import java.io.File
+
+val File.size get() = if (!exists()) 0.0 else length().toDouble()
+val File.sizeInKb get() = size / 1024
+val File.sizeInMb get() = sizeInKb / 1024
+val File.sizeInGb get() = sizeInMb / 1024
+val File.sizeInTb get() = sizeInGb / 1024
