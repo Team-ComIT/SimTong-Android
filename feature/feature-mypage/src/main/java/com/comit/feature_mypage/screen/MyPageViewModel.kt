44817d272ed7712624118e72bdf12733ab926f8e
limsaehyun <sh00710000@gmail.com>
2022-12-10 22:31:25 +0900

    feat: SIMT-76 파일 크기 초과 예외 처리

--- feature/feature-mypage/src/main/java/com/comit/feature_mypage/screen/MyPageViewModel.kt
+++ feature/feature-mypage/src/main/java/com/comit/feature_mypage/screen/MyPageViewModel.kt
@@ -2,6 +2,9 @@ package com.comit.feature_mypage.screen
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.comit.common.convert.limitSize
+import com.comit.common.unit.sizeInKb
+import com.comit.common.unit.sizeInMb
 import com.comit.domain.exception.NeedLoginException
 import com.comit.domain.usecase.users.ChangeProfileImageUseCase
 import com.comit.domain.usecase.users.FetchUserInformationUseCase
@@ -11,10 +14,13 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import org.orbitmvi.orbit.ContainerHost
 import org.orbitmvi.orbit.syntax.simple.intent
+import org.orbitmvi.orbit.syntax.simple.postSideEffect
 import org.orbitmvi.orbit.syntax.simple.reduce
 import org.orbitmvi.orbit.viewmodel.container
 import java.io.File
 import javax.inject.Inject
+
+internal const val ImageLimitSizeInKB: Int = 1024
 
 @HiltViewModel
 class MyPageViewModel @Inject constructor(
@@ -54,6 +60,11 @@ class MyPageViewModel @Inject constructor(
     fun changeProfileImage(
         profileImg: File,
     ) = intent {
+        if(profileImg.sizeInKb > ImageLimitSizeInKB) {
+            postSideEffect(MyPageSideEffect.LimitSize(profileImg.sizeInKb))
+            return@intent
+        }
+
         viewModelScope.launch {
             changeProfileImageUseCase(
                 profileImg = profileImg,
