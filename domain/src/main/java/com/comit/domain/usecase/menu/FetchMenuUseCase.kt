5ec827a3cd5250cba57806ea48ec18ea05654a34
limsaehyun <sh00710000@gmail.com>
2022-12-10 21:55:56 +0900

    feat: SIMT-68 meal entity mapper

--- domain/src/main/java/com/comit/domain/usecase/menu/FetchMenuUseCase.kt
+++ domain/src/main/java/com/comit/domain/usecase/menu/FetchMenuUseCase.kt
@@ -1,6 +1,8 @@
 package com.comit.domain.usecase.menu
 
+import com.comit.domain.model.MealEntity
 import com.comit.domain.repository.MenuRepository
+import com.comit.model.MenuList
 import java.util.Date
 import javax.inject.Inject
 
@@ -9,10 +11,18 @@ class FetchMenuUseCase @Inject constructor(
 ) {
 
     suspend operator fun invoke(
-        date: Date,
+        date: String,
     ) = kotlin.runCatching {
         repository.fetchMenu(
             date = date,
-        )
+        ).menu.map {
+            it.toEntity()
+        }
     }
 }
+
+fun MenuList.Menu.toEntity() =
+    MealEntity(
+        date = date,
+        food = meal.split(',')
+    )
