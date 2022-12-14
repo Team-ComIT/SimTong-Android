801ba4866cd62158b4d79d7b88fe877f9ca503ed
limsaehyun <sh00710000@gmail.com>
2022-12-14 19:50:10 +0900

    fix: SIMT-81 nickname format을 기본함수 형식으로 변경

--- common-util/src/main/java/com/comit/common/format/nickname.kt
+++ common-util/src/main/java/com/comit/common/format/nickname.kt
@@ -1,4 +1,1 @@
 package com.comit.common.format
-
-fun String.isNicknameFormat(): Boolean =
-    this.matches(RegexUtils.NICKNAME_PATTERN.toRegex())
--- common-util/src/main/java/com/comit/common/format/password.kt
+++ common-util/src/main/java/com/comit/common/format/password.kt
@@ -3,3 +3,6 @@ package com.comit.common.format
 fun isPasswordFormat(password: String): Boolean {
     return password.matches(RegexUtils.SECRET_PATTERN.toRegex())
 }
+
+fun isNicknameFormat(nickname: String): Boolean =
+    nickname.matches(RegexUtils.NICKNAME_PATTERN.toRegex())
