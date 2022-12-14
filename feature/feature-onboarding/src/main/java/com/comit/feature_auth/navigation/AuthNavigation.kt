66d06772e9b988d2fa9dcfc6cd901b8b4b57b74a
limsaehyun <sh00710000@gmail.com>
2022-12-14 13:29:51 +0900

    feat: SIMT-80 navigate error screen

--- feature/feature-onboarding/src/main/java/com/comit/feature_auth/navigation/AuthNavigation.kt
+++ feature/feature-onboarding/src/main/java/com/comit/feature_auth/navigation/AuthNavigation.kt
@@ -3,12 +3,16 @@ package com.comit.feature_auth.navigation
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavType
 import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
 import androidx.navigation.navigation
+import com.comit.feature_auth.screen.error.ErrorScreen
 import com.comit.feature_auth.screen.find.AuthFindScreen
 import com.comit.feature_auth.screen.signIn.SignInScreen
 import com.comit.feature_auth.screen.signUp.SignUpScreen
 import com.comit.feature_auth.screen.splash.StartScreen
+import com.comit.feature_auth.utils.AuthDeepLinkKeyUtil
 import com.comit.navigator.SimTongRoute
 import com.comit.navigator.SimTongScreen
 import com.google.accompanist.pager.ExperimentalPagerApi
@@ -50,6 +54,23 @@ fun NavGraphBuilder.authNavigation(
         ) {
             AuthFindScreen(
                 navController = navController,
+            )
+        }
+
+        composable(
+            route = SimTongScreen.Auth.ERROR + "/?${AuthDeepLinkKeyUtil.MESSAGE}={message}",
+            arguments = listOf(
+                navArgument(AuthDeepLinkKeyUtil.MESSAGE) {
+                    type = NavType.StringType
+                    defaultValue = ""
+                }
+            )
+        ) { backStackEntry ->
+            val message = backStackEntry.arguments?.getString(AuthDeepLinkKeyUtil.MESSAGE) ?: ""
+
+            ErrorScreen(
+                navController = navController,
+                errorMessage = message,
             )
         }
     }
