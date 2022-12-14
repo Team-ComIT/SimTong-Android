29fe26d5f8b733ac3413c6ab6e559909c40e447b
limsaehyun <sh00710000@gmail.com>
2022-12-14 17:24:28 +0900

    fix: SIMT-81 잘못된 정규식 수정

--- common-util/src/main/java/com/comit/common/format/nickname.kt
+++ common-util/src/main/java/com/comit/common/format/nickname.kt
@@ -1,5 +1,4 @@
 package com.comit.common.format
 
-fun isNickNameFormat(nickname: String): Boolean {
-    return nickname.matches("""(?=^\S)(?=^[\w\s가-힣.]{1,20}$).*(?=\S$).*""".toRegex())
-}
+fun String.isNicknameFormat(): Boolean =
+    this.matches(RegexUtils.NICKNAME_PATTERN.toRegex())
--- common-util/src/main/java/com/comit/common/format/password.kt
+++ common-util/src/main/java/com/comit/common/format/password.kt
@@ -1,5 +1,5 @@
 package com.comit.common.format
 
 fun isPasswordFormat(password: String): Boolean {
-    return password.matches("""(?=(.*[a-zA-Z].*)+)(?=(.*\d.*)+)(?=^[\w+\-$]{8,20}$).*""".toRegex())
+    return password.matches(RegexUtils.SECRET_PATTERN.toRegex())
 }
