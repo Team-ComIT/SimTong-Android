89a2186bb10b4b41e2f525fb48785cedf5faa37e
kimjjunho <kimjjun1007@gmail.com>
2022-12-28 20:41:56 +0900

    refactor: SIMT-92 휴무일 보기 및 일정 확인 뷰모델 에러처리

--- feature/feature-home/src/main/java/com/comit/feature_home/vm/GetWorkCountViewModel.kt
+++ feature/feature-home/src/main/java/com/comit/feature_home/vm/GetWorkCountViewModel.kt
@@ -4,6 +4,9 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.comit.domain.exception.BadRequestException
+import com.comit.domain.exception.UnAuthorizedException
+import com.comit.domain.exception.throwUnknownException
 import com.comit.domain.usecase.schedule.FetchPersonalScheduleUseCase
 import com.comit.feature_home.mvi.FetchScheduleState
 import com.comit.feature_home.mvi.toState
@@ -30,7 +33,11 @@ class GetWorkCountViewModel @Inject constructor(
             ).onSuccess {
                 _workCountList.value = it.toState().scheduleList
             }.onFailure {
-                _workCountList.value = listOf()
+                when (it) {
+                    is BadRequestException -> _workCountList.value = listOf()
+                    is UnAuthorizedException -> _workCountList.value = listOf()
+                    else -> throwUnknownException(it)
+                }
             }
         }
     }
