7276d383c2c24890ca3c3d69bd58ba3d2a30bb70
kimjjunho <kimjjun1007@gmail.com>
2023-01-11 09:11:59 +0900

    feat: SIMT-123 basicbtn clip 적용

--- core-design-system/src/main/java/com/comit/core_design_system/button/basicbutton.kt
+++ core-design-system/src/main/java/com/comit/core_design_system/button/basicbutton.kt
@@ -18,6 +18,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.Shape
@@ -67,6 +68,7 @@ fun BasicButton(
                 color = btnColor,
                 shape = shape,
             )
+            .clip(shape = shape)
             .runIf(enabled) {
                 composed {
                     simClickable(
